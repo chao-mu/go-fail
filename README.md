5c995f052cd6f8e13012cb7c53e9c28107b0634c
chao-mu <chao.mu@minorcrash.com>
2016-09-20 18:44:51 -0400

    Initial commit - you know you're lazy when you're writing commit messages in nano

---
+++ README.md
@@ -0,0 +1,7 @@
+A go library for when you just want to print to stderr and exit. Don't you deserve one less line of code?
+
+``` go
+if err := run(); err != nil {
+  fail.Failf("Discord detected: %v", err)
+}
+```
---
+++ exit.go
@@ -0,0 +1,29 @@
+package fail
+
+import (
+	"fmt"
+	"os"
+)
+
+// Fail prints the %v form of the provided value to stderr and exits with 1.
+func Fail(msg interface{}) {
+	Failf("%v", msg)
+}
+
+// Failf prints according to Printf rules to stderr and exits with 1.
+func Failf(pattern string, args ...interface{}) {
+	FailWithf(1, pattern, args...)
+}
+
+// Fail prints the %v form of the provided value to stderr and exits
+// with provided status code.
+func FailWith(code int, msg interface{}) {
+	FailWithf(code, "%v", msg)
+}
+
+// Failf prints according to Printf rules to stderr and exits with
+// provided status code.
+func FailWithf(code int, pattern string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, pattern, args...)
+	os.Exit(code)
+}
