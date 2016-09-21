d9e694c3bb8c5663504dab44eace4aac7875cfb1
chao-mu <chao.mu@minorcrash.com>
2016-09-21 12:37:52 -0400

    Support file handles

--- fail.go
+++ fail.go
@@ -12,18 +12,18 @@ func Fail(msg interface{}) {
 
 // Failf prints according to Printf rules to stderr and exits with 1.
 func Failf(pattern string, args ...interface{}) {
-	FailWithf(1, pattern, args...)
+	FailWithf(os.Stderr, 1, pattern, args...)
 }
 
-// FailWith prints the %v form of the provided value to stderr and exits
-// with provided status code.
-func FailWith(code int, msg interface{}) {
-	FailWithf(code, "%v", msg)
+// FailWith writes the %v form of the provided value to the provided
+// file handle ane exits with the provided status code.
+func FailWith(file *os.File, code int, msg interface{}) {
+	FailWithf(file, code, "%v", msg)
 }
 
-// FailWithf prints according to Printf rules to stderr and exits with
-// provided status code.
-func FailWithf(code int, pattern string, args ...interface{}) {
+// FailWithf writes according to Printf rules to provided file handle
+// and exits with the provided status code.
+func FailWithf(file *os.File, code int, pattern string, args ...interface{}) {
 	fmt.Fprintf(os.Stderr, pattern, args...)
 	os.Exit(code)
 }
