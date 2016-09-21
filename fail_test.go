ec80a92d0d5f39dfb3dca4b923079cac6990860e
chao-mu <chao.mu@minorcrash.com>
2016-09-21 13:55:28 -0400

    Add examples

---
+++ fail_test.go
@@ -0,0 +1,24 @@
+package fail_test
+
+import (
+	"errors"
+	"os"
+
+	fail "github.com/chao-mu/go-fail"
+)
+
+func ExampleFail() {
+	fail.Fail("I put all my money into Enron stocks")
+}
+
+func ExampleFailf() {
+	fail.Failf("Discord detected: %v", errors.New("Aaaah!"))
+}
+
+func ExampleWith() {
+	fail.With(os.Stdout, 23, "Hey there Standard out")
+}
+
+func ExampleWithf() {
+	fail.Withf(os.Stdout, 23, "Almost there: %s", "Cool")
+}
