06f8b9f9aea8449e907280ab005edd45ea9627e9
chao-mu <chao.mu@minorcrash.com>
2016-09-21 13:51:14 -0400

    Pass Go Lint, tweak doc, add newliens to print

--- fail.go
+++ fail.go
@@ -1,3 +1,5 @@
+// Package fail simplifies the process of printing/writing an
+// error message and then exiting.
 package fail
 
 import (
@@ -5,25 +7,40 @@ import (
 	"os"
 )
 
-// Fail prints the %v form of the provided value to stderr and exits with 1.
+const (
+	// Pattern dictates how Fail/With print the provided
+	// value.
+	Pattern = "%v"
+	// ErrorCode is the status code we return on exit
+	ErrorCode = 1
+)
+
+// format applys Pattern to msg via sprintf.
+func format(msg interface{}) string {
+	return fmt.Sprintf(Pattern, msg)
+}
+
+// Fail writes msg to stderr (according to fail.Pattern),
+// then exits with fail.ErrorCode.
 func Fail(msg interface{}) {
-	Failf("%v", msg)
+	Failf(Pattern, msg)
 }
 
-// Failf prints according to Printf rules to stderr and exits with 1.
+// Failf writes pattern to stderr parameterized by args according to printf
+// then exits with 1. A newline is appended.
 func Failf(pattern string, args ...interface{}) {
-	FailWithf(os.Stderr, 1, pattern, args...)
+	Withf(os.Stderr, ErrorCode, pattern, args...)
 }
 
-// FailWith writes the %v form of the provided value to the provided
-// file handle ane exits with the provided status code.
-func FailWith(file *os.File, code int, msg interface{}) {
-	FailWithf(file, code, "%v", msg)
+// With writes msg to provided file (according to fail.Pattern)
+// and exits with provided code.
+func With(file *os.File, code int, msg interface{}) {
+	Withf(file, code, Pattern, msg)
 }
 
-// FailWithf writes according to Printf rules to provided file handle
-// and exits with the provided status code.
-func FailWithf(file *os.File, code int, pattern string, args ...interface{}) {
-	fmt.Fprintf(os.Stderr, pattern, args...)
+// Withf writes pattern to the provided file parameterized by args
+// according to printf then exits with provided code. A newline is appended.
+func Withf(file *os.File, code int, pattern string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, pattern+"\n", args...)
 	os.Exit(code)
 }
