fbc0cf5d9c00f9cde4084cfcf0f1593afc0231a7
chao-mu <chao.mu@minorcrash.com>
2016-09-20 21:46:05 -0400

    Let's focus on failing

--- fail.go
+++ fail.go
@@ -12,18 +12,18 @@ func Fail(msg interface{}) {
 
 // Failf prints according to Printf rules to stderr and exits with 1.
 func Failf(pattern string, args ...interface{}) {
-	ExitWithf(1, pattern, args...)
+	FailWithf(1, pattern, args...)
 }
 
-// ExitWith prints the %v form of the provided value to stderr and exits
+// FailWith prints the %v form of the provided value to stderr and exits
 // with provided status code.
-func ExitWith(code int, msg interface{}) {
-	ExitWithf(code, "%v", msg)
+func FailWith(code int, msg interface{}) {
+	FailWithf(code, "%v", msg)
 }
 
-// ExitWithf prints according to Printf rules to stderr and exits with
+// FailWithf prints according to Printf rules to stderr and exits with
 // provided status code.
-func ExitWithf(code int, pattern string, args ...interface{}) {
+func FailWithf(code int, pattern string, args ...interface{}) {
 	fmt.Fprintf(os.Stderr, pattern, args...)
 	os.Exit(code)
 }
